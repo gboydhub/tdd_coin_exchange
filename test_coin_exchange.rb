5d005287c18ae0f919f95ace8dcd3ba8238a5e44
Eponick <mikehuntwife@gmail.com>
2018-06-21 20:59:49 -0400

    Test coin exchange returns hash, fail

---
+++ test_coin_exchange.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require_relative 'coin_exchange.rb'
+
+class TestCoinExchange < Minitest::Test
+  def test_assert_1_equals_1
+    assert_equal(1, 1)
+  end
+
+  def test_returns_hash
+    assert_equal({}, coin_exchange(0).class)
+  end
+end
