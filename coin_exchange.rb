efa0a922836641680f315270f2dec91e3c18389d
Eponick <mikehuntwife@gmail.com>
2018-06-21 21:17:21 -0400

    Test exchange returns nickel, pass

--- coin_exchange.rb
+++ coin_exchange.rb
@@ -1,3 +1,13 @@
 def coin_exchange(cents)
-  {:penny => 1}
+  coins_back = {}
+  while cents > 0 do
+    if cents >= 5
+      coins_back[:nickel] = 1
+      cents -= 5
+    elsif cents >= 1
+      coins_back[:penny] = 1
+      cents -= 1
+    end
+  end
+  coins_back
 end
