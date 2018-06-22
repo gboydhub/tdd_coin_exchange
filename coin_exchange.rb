64072e636bd79509996a397704d7e8de17c3635c
Eponick <mikehuntwife@gmail.com>
2018-06-21 21:19:04 -0400

    Test exchange returns dime, pass

--- coin_exchange.rb
+++ coin_exchange.rb
@@ -1,7 +1,10 @@
 def coin_exchange(cents)
   coins_back = {}
   while cents > 0 do
-    if cents >= 5
+    if cents >= 10
+      coins_back[:dime] = 1
+      cents -= 10
+    elsif cents >= 5
       coins_back[:nickel] = 1
       cents -= 5
     elsif cents >= 1
